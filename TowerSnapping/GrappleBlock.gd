dbc6bf0526df6315e897d5925c1511f5d558fd12
Mykyta Dniprovskyi <dnipromv@gmail.com>
2019-07-19 22:40:15 +0200

    implemented grapple block sensor area

---
+++ TowerSnapping/GrappleBlock.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+onready var collision_sensor = get_node("CollisionSensor");
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+func _physics_process(dt):
+	print(collision_sensor.get_overlapping_bodies());
+	pass
--- TowerSnapping/GrappleMover.gd
+++ TowerSnapping/GrappleMover.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func _process(dt):
+func _physics_process(dt):
 	var mouse_position = get_viewport().get_mouse_position();
 	self.position.x = mouse_position.x;
 	self.position.y = mouse_position.y;
