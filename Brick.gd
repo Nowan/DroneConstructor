710fbbe5ee0a4459141be14708bb693e9f3310ba
Michał Łazowik <mlazowik@gmail.com>
2019-07-19 23:39:28 +0200

    Add brick factory

--- Brick.gd
+++ Brick.gd
@@ -1,10 +1,13 @@
-extends Node
+extends Node2D
 
 export (PackedScene) var Brick
 
 const UNIT_SIZE = 10
 const MAX_BRICKS = 8
 const MAX_DIM_DIFF = 3
+
+onready var collision = get_node("Collision")
+onready var color = get_node("ColorRect")
 
 func _ready():
 	randomize()
@@ -21,14 +24,33 @@ func _ready():
 	var max_width = min(height + MAX_DIM_DIFF, int(MAX_BRICKS / height))
 	var width = randi() % (max_width - min_width + 1) + min_width
 	
-	var body = get_node("Body")
-	var collision = body.get_node("Collision")
-	var color = body.get_node("ColorRect")
-	
-	collision.shape.extents.x = width * UNIT_SIZE
-	collision.shape.extents.y = height * UNIT_SIZE
+	var collision_shape = RectangleShape2D.new()
+	collision_shape.extents.x = width * UNIT_SIZE
+	collision_shape.extents.y = height * UNIT_SIZE
+	collision.shape = collision_shape
 
 	color.margin_left = -width * UNIT_SIZE
 	color.margin_right = width * UNIT_SIZE
 	color.margin_top = -height * UNIT_SIZE
 	color.margin_bottom = height * UNIT_SIZE
+
+var pressed = false
+
+func _on_ColorRect_gui_input(event):
+	if event is InputEventMouseButton:
+		pressed = event.pressed
+	elif event is InputEventMouseMotion:
+		if false:
+			#collision.position = event.position
+			#color.position = event.position
+			var force = event.global_position - self.position
+			print(force)
+			self.applied_force = 100 * force
+
+func _process(data):
+	if pressed:
+		var force = get_viewport().get_mouse_position() - self.position
+		#print(force)
+		self.applied_force = force
+	else:
+		self.applied_force = Vector2(0, 0)
--- Main.gd
+++ Main.gd
@@ -2,8 +2,34 @@ extends Node
 
 export (PackedScene) var Brick
 
-func _input(event):
-	if event.is_action_pressed("click"):
-		var new_brick = Brick.instance()
-		new_brick.position = get_viewport().get_mouse_position()
-		add_child(new_brick)
+const NUM_BLOCKS = 3
+const SPACING = 100
+const OFFSET = 600
+
+var watchers = []
+
+func _ready():
+	for block_id in range(NUM_BLOCKS):
+		watchers.append(create_watcher(block_id))
+		create_brick(block_id)
+
+func _process(data):
+	for block_id in range(NUM_BLOCKS):
+		if !watchers[block_id].is_colliding():
+			create_brick(block_id)
+
+func create_watcher(id: int):
+	var new_watcher = RayCast2D.new()
+	new_watcher.position = Vector2(get_position(id), 500)
+	new_watcher.cast_to = Vector2(0, 99)
+	new_watcher.enabled = true
+	add_child(new_watcher)
+	return new_watcher
+
+func create_brick(id: int):
+	var new_brick = Brick.instance()
+	new_brick.position = Vector2(OFFSET + id * SPACING, 599)
+	add_child(new_brick)
+
+func get_position(id: int):
+	return OFFSET + id * SPACING
